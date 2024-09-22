aa31e79667374a093709f6fc7edbcc01a443c43f
llindemann <leonie.lindemann.ll@gmail.com>
2024-09-22 20:27:56 +0200

    feat: Moveable Camera

---
+++ Camera2D.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+const SPEED = 5.0
+	
+func _process(_delta):
+	# Get the input direction and handle the camera movement
+	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
+	position += direction.x * SPEED
