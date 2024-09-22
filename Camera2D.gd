05dee20a357a474837eb8b29b7f1f47a855ab557
llindemann <leonie.lindemann.ll@gmail.com>
2024-09-22 20:42:39 +0200

    fix: false operator in Camera2D Script

--- Camera2D.gd
+++ Camera2D.gd
@@ -4,4 +4,4 @@ const SPEED = 5.0
 func _process(_delta):
 	# Get the input direction and handle the camera movement
 	var direction = Input.get_vector("ui_left", "ui_right",  "ui_up", "ui_down")
-	position += direction.x * SPEED
+	position += direction * SPEED
